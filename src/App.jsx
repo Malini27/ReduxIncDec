63b57c0bd7b3a267e3b78e1652aea8ede03688af
malini <maliniraj9025565842@gmail.com>
2023-09-10 09:24:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,218 +1,72 @@
 import './App.css'
 function  App(){
+    document.addEventListener('DOMContentLoaded',()=>{
+        let burger=document.querySelector(".burger");
+        let links=document.querySelector(".nav_links");
+        let txtarea=document.querySelector(".text_area");
+
+        burger.addEventListener('click',()=> {
+           links.classList.toggle("nav_show");
+           txtarea.classList.toggle("textareahide");
+
+    })
+
+    })
     return(
-        <div className="container">
-            <h1 className="heading">SURVEY FORM</h1>
-            <hr></hr>
-            <form className='form_class'method='get'>
-                <div className='heading_field'>
-                <h3>First Name</h3>
-                <input type="text"className='field' required></input>
+        <>
+        <header>
+            <div className='header_content'>
+            <div className='logo'>
+                <img src='th.png' alt=''></img>
             </div>
-            <div className='heading_field'>
-                <h3>Last Name</h3>
-                <input type="text" className='field'required></input>
+            <ul className='nav_links'>
+                <li><a href='a'>Home</a></li>
+                <li><a href='a'>About</a></li>
+                <li><a href='a'>Courses</a></li>
+                <li><a href='a'>Contact</a></li>
+            </ul>
+            <div className='burger'>
+            <i className="fa-solid fa-bars" style={{color: "#f7f7f7",}} />
+               
             </div>
-            <div className='heading_field'>
-                <h3>Gender</h3>
-                <input type="radio" name="gender"id="male"></input>
-                <label for="male">Male</label><br></br>
-                <input type="radio" name="gender"id="female"></input>
-                <label for="female">Female</label>
+
             </div>
-            <div className='form_control'>
-                <div className='heading_field'>
-                    <h3>Address</h3>
-                </div>
-                <div>
-                   <div className='input_div'>
-                   <input type="text" className='form_control_input'required></input>
-                   </div>
-               </div>
+            <div className='text_center'>
+                <h3>Welcome To</h3>
+                <h1>RJ College Of Engineering</h1>
+                <a className='btn' href="a">Register Here</a>
             </div>
-            <div className='form_control'>
-                <div className='heading_field'>
-                    <h3>City</h3>
+        </header>
+
+        <main>
+            <section className='wwa content mt mb'>
+                <div className='wwa_heading'>
+                    <h2>About Us</h2>
                 </div>
-                <input type="text"className='field'></input>
-                <div className='heading_field'>
-                    <h3>State</h3>
+                <div className='wwa_content flex_design'>
+                    <div className='vision wwa_box'>
+                        <h3>Vision</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+
+                    </div>
+                    <div className='mission wwa_box'>
+                        <h3>Mission</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div className='whoweare wwa_box'>
+                        <h3>Who We Are</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </div>
                 </div>
-                <input type="text"className='field'></input>
-                <div className='heading_field'>
-                    <h3>Pincode</h3>
-                </div>
-                <input type="text"className='field'></input>
-            </div>
-            <div className='form_control'>
-                <div className='heading_field'>
-                    <h3>Country</h3>
-                </div>
-                <select className='field' >
-                    <option value="Afghanistan">Afghanistan</option>
-                    <option value="Albania">Albania</option>
-                    <option value="Algeria">Algeria</option>
-                    <option value="American Samoa">American Samoa</option>
-                    <option value="Andorra">Andorra</option>
-                    <option value="Angola">Angola</option>
-                    <option value="Anguilla">Anguilla</option>
-                    <option value="Aruba">Aruba</option>
-                    <option value="Australia">Australia</option>
-                    <option value="Austria">Austria</option>
-                    <option value="Azerbaijan">Azerbaijan</option>
-                    <option value="Bahamas">Bahamas</option>
-                    <option value="Bahrain">Bahrain</option>
-                    <option value="Bangladesh">Bangladesh</option>
-                    <option value="Barbados">Barbados</option>
-                    <option value="Belarus">Belarus</option>
-                    <option value="Belgium">Belgium</option>
-                    <option value="Belize">Belize</option>
-                    <option value="Benin">Benin</option>
-                    <option value="Bermuda">Bermuda</option>
-                    <option value="Bhutan">Bhutan</option>
-                    <option value="Bolivia">Bolivia</option>
-                    <option value="Bosnia and Herzegowina">Bosnia and Herzegowina</option>
-                    <option value="Botswana">Botswana</option>
-                    <option value="Bouvet Island">Bouvet Island</option>
-                    <option value="Brazil">Brazil</option>
-                    <option value="British Indian Ocean Territory">British Indian Ocean Territory</option>
-                    <option value="Brunei Darussalam">Brunei Darussalam</option>
-                    <option value="Bulgaria">Bulgaria</option>
-                    <option value="Burkina Faso">Burkina Faso</option>
-                    <option value="Burundi">Burundi</option>
-                    <option value="Cambodia">Cambodia</option>
-                    <option value="Cameroon">Cameroon</option>
-                    <option value="Canada">Canada</option>
-                    <option value="Cape Verde">Cape Verde</option>
-                    <option value="Cayman Islands">Cayman Islands</option>
-                    <option value="Central African Republic">Central African Republic</option>
-                    <option value="Chad">Chad</option>
-                    <option value="Chile">Chile</option>
-                    <option value="China">China</option>
-                    <option value="Christmas Island">Christmas Island</option>
-                    <option value="Cocos Islands">Cocos (Keeling) Islands</option>
-                    <option value="Colombia">Colombia</option>
-                    <option value="Comoros">Comoros</option>
-                    <option value="Congo">Congo</option>
-                    <option value="India">India</option>
+            </section>
+        </main>
+        </>
 
         
-                 </select>
-                 <div className='heading_field'>
-                    <h3>Phone Number</h3>
-                 </div>
-                 <input type="text"className='field'></input>
-                 <div className='heading_field'>
-                    <h3>Email</h3>
-                 </div>
-                 <input type="email"className='field'></input>
-            </div>
-            <div className='form_control'>
-                <div className='heading_field'>
-                <div className='input_field'>
-                    <h4>Product1</h4>
-                    <div class="input-div">
-                        <input type="radio" id="bad" name="rating1" value="bad"></input>
-                        <label for="bad">Bad</label><br></br>
-                        <input type="radio" id="bad" name="rating1" value="good"></input>
-                        <label for="bad">Good</label><br></br>
-                        <input type="radio" id="bad" name="rating1" value="excellence"></input>
-                        <label for="bad">Excellent</label>
-
-                    </div>
-                    
-
-                </div>
-                <div className='heading_field'>
-                <div className='input_field'>
-                    <h4>Product2</h4>
-                    <div class="input-div">
-                        <input type="radio" id="bad2" name="rating2" value="bad"></input>
-                        <label for="bad2">Bad</label><br></br>
-                        <input type="radio" id="good2" name="rating2" value="good"></input>
-                        <label for="good2">Good</label><br></br>
-                        <input type="radio" id="ex2" name="rating2" value="excellence"></input>
-                        <label for="ex2">Excellent</label>
-
-                    </div>
-                    
-
-                </div>
-
-                </div>
-                <div className='heading_field'>
-                <div className='input_field'>
-                    <h4>Product3</h4>
-                    <div class="input-div">
-                        <input type="radio" id="bad3" name="rating3" value="bad"></input>
-                        <label for="bad3">Bad</label><br></br>
-                        <input type="radio" id="good3" name="rating3" value="good"></input>
-                        <label for="good3">Good</label><br></br>
-                        <input type="radio" id="ex3" name="rating3" value="excellence"></input>
-                        <label for="ex3">Excellent</label>
-
-                    </div>
-                    
-
-                </div>
-
-                </div>
-                <div className='heading_field'>
-                <div className='input_field'>
-                    <h4>Product4</h4>
-                    <div class="input-div">
-                        <input type="radio" id="bad4" name="rating4" value="bad"></input>
-                        <label for="bad4">Bad</label><br></br>
-                        <input type="radio" id="good4" name="rating4" value="good"></input>
-                        <label for="good4">Good</label><br></br>
-                        <input type="radio" id="ex4" name="rating4" value="excellence"></input>
-                        <label for="ex4">Excellent</label>
-
-                    </div>
-                    
-
-                </div>
-
-                </div>
-                <div className='heading_field'>
-                <div className='input_field'>
-                    <h4>Product5</h4>
-                    <div class="input-div">
-                        <input type="radio" id="bad5" name="rating5" value="bad"></input>
-                        <label for="bad5">Bad</label><br></br>
-                        <input type="radio" id="good5" name="rating5" value="good"></input>
-                        <label for="good5">Good</label><br></br>
-                        <input type="radio" id="ex5" name="rating5" value="excellence"></input>
-                        <label for="ex5">Excellent</label>
-
-                    </div>
-                    
-
-                </div>
-
-                </div>
-
-                
-
-                    
-
-                </div>
-            </div>
-            <div className='heading_field'>
-                <button type='submit' className='button'>Submit</button>
-
-            </div>
-        
-            
+    
+    
 
         
-            
-                
-
-            </form>
-           
-
-        </div>    
     )
 }
 export default App;
