5464959664fbaadfa689e3dd5484f0ac815f339a
malini <maliniraj9025565842@gmail.com>
2023-09-13 14:10:32 +0530

    assign2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,32 @@
+import { useState } from 'react';
 import './App.css'
 function  App(){
+    const[num1,setnum1]=useState('');
+    const[num2,setnum2]=useState('');
+    const[sum,setsum]=useState('');
+    
+    const calculateSum=()=>{
+         const result=Number(num1)+Number(num2)
+         setsum(result)
+
+    }
+
     
     return(
-        <>
-        <main>
-        <div id='landing'>
-            <div id='landing_text'>
-                <div id='landing_text_inner'>
-                    <h1>My Photography Website</h1>
-                    
-                </div>
+        <div>
+            <h2>sum calculator:</h2>
+            <div>
+               
+                <input type="Number" value={num1} className="n1"onChange={(e) => setnum1(e.target.value)}></input>
+                <input type="Number" value={num2} className='n1' onChange={(e) => setnum2(e.target.value)}></input>
             
-
+            <button className='btn' onClick={calculateSum}>+</button>
+            <p className='n1'>Sum: {sum}</p>
             </div>
         </div>
-            <div id='images'>
-                <div id='header'>
-                        <h3>My Work</h3>
-
-                </div>
-                    <img src='photo1.png' alt='natural image' className='image_class'></img>
-                    <div className='caption'>
-                        <h3>Photo 1</h3>
-                        <p>Sunset Beach View</p>
-                    </div>
-                    <img src='photo2.png'alt='natural image'className='image_class'></img>
-                    <div className='caption'>
-                        <h3>Photo 2</h3>
-                        <p>Beautiful Blue Ocean</p>
-                    </div>
-                    <img src='photo3.png'alt='natural image' className='image_class'></img>
-                    <div className='caption'>
-                        <h3>Photo 3</h3>
-                        <p>Autumn Fall</p>
-                    </div>
-                    <img src='photo4.png'alt='natural image'className='image_class'></img>
-                    <div className='caption'>
-                        <h3>Photo 4</h3>
-                        <p>Lovable Lavender Field</p>
-                    </div>
-                    <img src='photo5.png'alt='natural image' className='image_class'></img>
-                    <div className='caption'>
-                        <h3>Photo5</h3>
-                        <p>Full Moon View</p>
-                    </div>
-
-            </div>
-
-        </main>
-        
-            <footer>
-                <h3 className='foot'>Get In Touch</h3>
-                <p className='foot1'>Email or Call me to set up a consult</p>
-                <p className='foot2'>hello@gmail.com</p>
-                <p className='foot2'>8928738974</p>
-            </footer>
         
 
-        </>
-        
     )
+        
 }
 export default App;
