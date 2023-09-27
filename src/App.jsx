386c6b0ba11d84c8c7a69323c350c07f643dbf7b
malini <maliniraj9025565842@gmail.com>
2023-09-27 08:00:29 +0530

    m3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
-function  App(){
-    const[num1,setnum1]=useState('');
-    const[num2,setnum2]=useState('');
-    const[sum,setsum]=useState('');
-    
-    const calculateSum=()=>{
-         const result=Number(num1)+Number(num2)
-         setsum(result)
+function App(){
+    const cities=['aandipati','borur','cuddalore','cuddapah','dindugal','pondy','villupuram']
 
+    const[search,setsearch]=useState('');          //declaring two state variable(search,setsearch) using usestate hook with empty string
+
+    const[filtered,setfiltered]=useState(cities);  //declaring two state variables and initializing with cities array
+
+    const onchangehandler=(event)=>{        // It updates the search state with the current value of the input field when the user types into it.
+        setsearch(event.target.value)
     }
 
-    
-    return(
-        <div className='div1'>
-            <h2>sum calculator:</h2>
-            <div>
-               
-                <input type="Number" value={num1} className="n1"onChange={(e) => setnum1(e.target.value)}></input>
-                <input type="Number" value={num2} className='n1' onChange={(e) => setnum2(e.target.value)}></input>
-            
-            <button className='btn' onClick={calculateSum}>+</button>
-            <p >Sum: {sum}</p>
-            </div>
-        </div>
-        
+    useEffect(()=>{     //useEffect hook to run a side effect when the search state changes.
+        const filtered=[...cities].filter(
+            (element)=>(element?.toLocaleLowerCase()?.startsWith(search.toLocaleLowerCase()))  // filtering the cities array based on whether each city starts with the lowercase version of the search value. This will update the list of cities that match the search criteria.
 
-    )
+        )
+            setfiltered(filtered)
+
         
+    }
+    ,[search])
+
+
+
+return(
+    <div className='container'>
+        <h1>Search</h1>
+        <input className='searchbar' placeholder='type to search'value={search} onChange={onchangehandler}></input>
+        <ol className='searchlist'>
+            {
+                filtered.map((element,idx)=>{
+                    return(
+                        <li className='result'key={idx} value={element}>{element}</li>
+                    )
+
+                })
+            }
+
+        </ol>
+    </div>
+)
 }
 export default App;
