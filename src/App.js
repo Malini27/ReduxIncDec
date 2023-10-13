fcd4fb37ffb9462a9406f2b03b04587302334dbf
malini <maliniraj9025565842@gmail.com>
2023-10-13 13:02:36 +0530

    m5 commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,76 @@
-import react,{createContext, useState } from 'react';
-import { Namecard } from './component/Namecard';
+import React from 'react';
+import { BrowserRouter,Route,Link, Routes } from 'react-router-dom';
 import './App.css';
 
-export const ReusableFunction=createContext();   //creating the context (this will be shared among children)
+const Menu=()=>{
+    return (
+    <div>
+        <ul className='ul1'>
+            <li>Chicken Briyani</li>
+            <li>Mutton Briyani</li>
+            <li>Chicken Pasta</li>
+            <li>Veg Pasta</li>
+            <li>Pizza</li>
+            <li>Burger</li>
+        </ul>
+    </div>
+    )
+}
+const ComboOffer=()=>{
+    return (
+    <div>
+        <ul className='ul2'>
+            <li>Chicken Briyani+Chicken lollipop</li>
+            <li>Mutton Briyani+Tandoori</li>
+            <li>Pizza+Coke</li>
+            <li>Burger+ThumbsUP</li>
+        </ul>
+    </div>
+    )
+}
+const About=()=>{
+    return (
+        <div className='ul3'>
+            <p>For Order Contact Us : 7823781234 </p>
+            <p>Email : kenrestaurant@gmail.com</p>
+        </div>
+    )
+}
 
+const Navigation=()=>{
+    return (
+    <div className='container'>
+        <p><b>KEN RESTAURANT</b></p>
+        <hr></hr>
+    <nav>
+        <ul className='navi'>
+            <li className='li2'><Link to='/menu'>Menu</Link></li>
+            <li className='li2'><Link to='/combo'>Combo Offer</Link></li>
+            <li className='li2'><Link to='/about'>About</Link></li>
+
+        </ul>
+    </nav>
+    </div>
+    );
+};
 
 const App=()=>{
-    const[name,setname]=useState('')
-    const[age,setage]=useState('')
-
-    const getInfo={
-        getName:()=>{
-            return `Name: ${name} Age: ${age}`
-        },
-        getResult:()=>{
-            return `Your name is ${name} and your age is ${age}`
-        }
-
-    }
-    
 return(  
-    <div className='container'>
-    <input type='text' className='input1' value={name} placeholder='Enter your name'onChange={(e)=>setname(e.target.value)}></input><br></br>
-    <input type='Number'className='input2'value={age} placeholder='Enter your age'onChange={(e)=>setage(e.target.value)}></input>
-    <ReusableFunction.Provider value={getInfo}>
-    <Namecard name={name} age={age} />
-    </ReusableFunction.Provider>
+    <>
+    <BrowserRouter>
+      <div>
+        <Navigation/>
+        <Routes>
+            <Route path='/menu' element={<Menu/>} />
+            <Route path='/combo' element={<ComboOffer/>} />
+            <Route path='/about' element={<About/>} />
+
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
+    </>
     
-</div>
 );
 }
 export default App;
