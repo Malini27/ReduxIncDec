364a611e3cea4484c204c9e8ab284fb0aa02f406
malini <maliniraj9025565842@gmail.com>
2023-10-29 19:37:50 +0530

    m9 commit

--- src/App.js
+++ src/App.js
@@ -1,66 +1,52 @@
 import React from 'react';
-import { useState } from 'react';
-import './App.css';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official';
+// import './App.css';
 
 const App=()=>{
-    const[display,setDisplay]=useState(0)
-    const[operand,setOperand]=useState('')
-    const[operator,setOperator]=useState('')
-
-    const handleOperandClick=(digit)=>{
-        if(display=== '0'||operator){
-            setDisplay(digit)
-        }
-        else{
-            setDisplay(display+digit)
-        }
-
-    }
-    const handleEqualClick=()=>{
-        console.log(`${operand} ${operator} ${display}`)
-        // eslint-disable-next-line no-eval
-        const result = eval(`${parseFloat(operand)} ${operator}${parseFloat(display)}`);
-        setDisplay(String(result));
-        setOperand('');
-        setOperator('')
-    }
-    const handleOperatorClick=(SelectedOperator)=>{
-        if(!operator){
-            setOperand(display);
-            setOperator(SelectedOperator);
-        }
-
-    }
-
+    const options ={
+        chart: {
+        type: 'pie'
+      },
+      title: {
+        text: 'Engineering Department'
+      },
+       series: [
+        {
+        name: 'Students',
+        colorByPoint: true,
+        data: [
+          {
+            name: 'computer Science',
+            y: 39.5,
+            sliced: true,
+            selected: true
+          },
+          {
+            name: 'Information Technology',
+            y: 18.34
+          },
+          {
+            name: 'Electronics and Communication',
+            y: 20.2
+          },
+          {
+            name: 'civil',
+            y: 10.23
+          },
+          {
+            name: 'Mechanic',
+            y:11.73
+          }
+          ]
+      }]
+    };
     
     
 return( 
-    <div className='container'>
-    <div className='calculator'>
-        <div className='display'>{display}</div>
-        <div className='buttons'>
-            {
-                [7,8,9,'+',4,5,6,'-',1,2,3,'*',0,'=','/'].map(items=>{
-                    return<button key={items}
-                    onClick={()=>{
-                        if(typeof items ==='number'){
-                            handleOperandClick(String(items));
-                        }
-                        else if(items === '='){
-                            handleEqualClick();
-                        }
-                        else{
-                            handleOperatorClick(items);
-                        }
-                    }}
-                    >{items}</button>
-                })
-            }
-            {
-                <button onClick={()=>{setDisplay('0');setOperand('')}}>AC</button>
-            }
-        </div>
-    </div>
+    <div>
+        <HighchartsReact highcharts={Highcharts} options={options}/>
+
     </div>
     
 );
